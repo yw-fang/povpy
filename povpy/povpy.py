d1b5e28d690ba748ee6fb3bb939696cc28ba1b73
yw-fang <fyuewen@gmail.com>
2018-12-24 17:39:16 +0900

    povpy.py first commit, copyright information

---
+++ povpy/povpy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2018, Yue-Wen FANG.
+License: BSD 3-Clause License (see LICENSE for details)
+"""
+
+
+__author__ = 'Yue-Wen FANG'
+__maintainer__ = "Yue-Wen FANG"
+__email__ = 'fyuewen@gmail.com'
+__version__ = '0.01-dev'
+__license__ = 'BSD 3-Clause License'
+__creation_date__= 'Dec. 24, 2018'
+
+
